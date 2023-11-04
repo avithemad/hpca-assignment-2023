500703b553c331c3e888e003c013ffd7795cf563
avinashd <avithemad@gmail.com>
2023-11-04 11:33:05 +0530

    Implemented 3 optimizations for single thread, 1. Use temp, 2. Loop unroll(best), 3. Reorder loop

--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -1,32 +1,78 @@
 // Optimize this function
-void singleThread( int input_row, 
-                int input_col,
-                int *input, 
-                int kernel_row, 
-                int kernel_col, 
-                int *kernel,
-                int output_row, 
-                int output_col, 
-                long long unsigned int *output ) 
+void singleThread(int input_row,
+                  int input_col,
+                  int *input,
+                  int kernel_row,
+                  int kernel_col,
+                  int *kernel,
+                  int output_row,
+                  int output_col,
+                  long long unsigned int *output)
 {
-    
-    for(int i = 0; i < output_row * output_col; ++i)
+
+    for (int i = 0; i < output_row * output_col; ++i)
         output[i] = 0;
 
-    for(int output_i = 0; output_i< output_row; output_i++)
+    // Optimization 1, use a temporary variable to store the output
+    // for (int output_i = 0; output_i < output_row; output_i++)
+    // {
+    //     for (int output_j = 0; output_j < output_col; output_j++)
+    //     {
+    //         long long unsigned int temp = 0;
+    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
+    //         {
+    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
+    //             {
+    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
+    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
+    //                 temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+    //             }
+    //         }
+    //         output[output_i * output_col + output_j] = temp;
+    //     }
+    // }
+
+    for (int output_i = 0; output_i < output_row; output_i += 2)
     {
-        for(int output_j = 0; output_j< output_col; output_j++)
+        for (int output_j = 0; output_j < output_col; output_j += 2)
         {
-            for(int kernel_i = 0; kernel_i< kernel_row; kernel_i++)
+            long long unsigned int temp = 0;
+            long long unsigned int temp2 = 0;
+            long long unsigned int temp3 = 0;
+            long long unsigned int temp4 = 0;
+            for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
             {
-                for(int kernel_j = 0; kernel_j< kernel_col; kernel_j++)
+                for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
                 {
-                    int input_i = (output_i + 2*kernel_i) % input_row;
-                    int input_j = (output_j + 2*kernel_j) % input_col;
-                    output[output_i * output_col + output_j] += input[input_i*input_col +input_j] 
-                                                                * kernel[kernel_i*kernel_col +kernel_j];
+                    int input_i = (output_i + 2 * kernel_i) % input_row;
+                    int input_j = (output_j + 2 * kernel_j) % input_col;
+                    temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+                    temp2 += input[input_i * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
+                    temp3 += input[(input_i + 1) * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+                    temp4 += input[(input_i + 1) * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
                 }
             }
+            output[output_i * output_col + output_j] = temp;
+            output[output_i * output_col + output_j + 1] = temp2;
+            output[(output_i + 1) * output_col + output_j] = temp3;
+            output[(output_i + 1) * output_col + output_j + 1] = temp4;
         }
     }
+
+    // Optimization 3
+    // for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
+    // {
+    //     for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
+    //     {
+    //         for (int output_i = 0; output_i < output_row; output_i++)
+    //         {
+    //             for (int output_j = 0; output_j < output_col; output_j++)
+    //             {
+    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
+    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
+    //                 output[output_i * output_col + output_j] += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+    //             }
+    //         }
+    //     }
+    // }
 }
