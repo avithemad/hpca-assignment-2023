58b7fd22f4fa97806e84fef2cfc54e2690665b25
avinashd <avithemad@gmail.com>
2023-11-05 11:00:56 +0530

    Cleaned out single and multi thread files

--- PartA/header/multi_thread.h
+++ PartA/header/multi_thread.h
@@ -2,7 +2,7 @@
 #include <emmintrin.h>
 
 // Define the number of threads
-#define NUM_THREADS 16 // You can adjust this based on the number of CPU cores
+#define NUM_THREADS 8 // You can adjust this based on the number of CPU cores
 
 // Define a structure to hold parameters for each thread
 typedef struct
@@ -36,92 +36,26 @@ void *convolution_thread(void *arg)
     int output_col = params->output_col;
     long long unsigned int *output = params->output;
 
-    // Calculate the portion of the output matrix to compute for this thread
-    int output_rows_per_thread = output_row / NUM_THREADS;
-    int start_row = thread_id * output_rows_per_thread;
-    int end_row = (thread_id == NUM_THREADS - 1) ? output_row : start_row + output_rows_per_thread;
-
-    // for (int output_i = thread_id; output_i < output_row; output_i += NUM_THREADS)
-    // {
-    //     for (int output_j = 0; output_j < output_col; output_j++)
-    //     {
-    //         long long unsigned int temp = 0;
-    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    //         {
-    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-    //                 temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //             }
-    //         }
-    //         output[output_i * output_col + output_j] = temp;
-    //     }
-    // }
-
-    // for (int output_i = thread_id; output_i < end_row; output_i += 2 * NUM_THREADS)
-    // {
-    //     for (int output_j = 0; output_j < output_col; output_j += 2)
-    //     {
-    //         long long unsigned int temp = 0;
-    //         long long unsigned int temp2 = 0;
-    //         long long unsigned int temp3 = 0;
-    //         long long unsigned int temp4 = 0;
-    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    //         {
-    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-    //                 temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 temp2 += input[input_i * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 temp3 += input[(input_i + 1) * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 temp4 += input[(input_i + 1) * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
-    //             }
-    //         }
-    //         output[output_i * output_col + output_j] = temp;
-    //         output[output_i * output_col + output_j + 1] = temp2;
-    //         output[(output_i + 1) * output_col + output_j] = temp3;
-    //         output[(output_i + 1) * output_col + output_j + 1] = temp4;
-    //     }
-    // }
-    for (int output_i = thread_id; output_i < end_row; output_i += 2 * NUM_THREADS)
+    for (int output_i = thread_id; output_i < output_row; output_i += NUM_THREADS)
     {
         for (int output_j = 0; output_j < output_col; output_j += 2)
         {
-            __m128i temp1 = _mm_setzero_si128();
-            __m128i temp2 = _mm_setzero_si128();
-            __m128i temp3 = _mm_setzero_si128();
-            __m128i temp4 = _mm_setzero_si128();
-
+            long long unsigned int temp = 0;
+            long long unsigned int temp2 = 0;
             for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
             {
                 for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
                 {
                     int input_i = (output_i + 2 * kernel_i) % input_row;
                     int input_j = (output_j + 2 * kernel_j) % input_col;
-
-                    __m128i input_values = _mm_set_epi16(input[input_i * input_col + input_j + 1],
-                                                         input[input_i * input_col + input_j],
-                                                         input[(input_i + 1) * input_col + input_j + 1],
-                                                         input[(input_i + 1) * input_col + input_j],
-                                                         0, 0, 0, 0);
-
-                    __m128i kernel_values = _mm_set1_epi16(kernel[kernel_i * kernel_col + kernel_j]);
-
-                    temp1 = _mm_add_epi32(temp1, _mm_mullo_epi16(input_values, kernel_values));
-
-                    // Similar calculations for temp2, temp3, and temp4
+                    temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+                    temp2 += input[input_i * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
                 }
             }
-
-            long long unsigned int result[4];
-            _mm_storeu_si128((__m128i *)result, temp1);
-            output[output_i * output_col + output_j] = result[0];
-            output[output_i * output_col + output_j + 1] = result[1];
+            output[output_i * output_col + output_j] = temp;
+            output[output_i * output_col + output_j + 1] = temp2;
         }
     }
-
     pthread_exit(NULL);
 }
 
--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -12,28 +12,10 @@ void singleThread(int input_row,
                   long long unsigned int *output)
 {
 
-    for (int i = 0; i < output_row * output_col; ++i)
-        output[i] = 0;
+    // for (int i = 0; i < output_row * output_col; ++i)
+    //     output[i] = 0;
 
-    // Optimization 1, use a temporary variable to store the output
-    // for (int output_i = 0; output_i < output_row; output_i++)
-    // {
-    //     for (int output_j = 0; output_j < output_col; output_j++)
-    //     {
-    //         long long unsigned int temp = 0;
-    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    //         {
-    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-    //                 temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //             }
-    //         }
-    //         output[output_i * output_col + output_j] = temp;
-    //     }
-    // }
-
+    // Optimization 4, compute in blocks for 4 to exploit locality, use modulus sparingly
     for (int output_i = 0; output_i < output_row; output_i += 2)
     {
         for (int output_j = 0; output_j < output_col; output_j += 2)
@@ -46,8 +28,10 @@ void singleThread(int input_row,
             {
                 for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
                 {
-                    int input_i = (output_i + 2 * kernel_i) % input_row;
-                    int input_j = (output_j + 2 * kernel_j) % input_col;
+                    int i = output_i + 2 * kernel_i, j = output_j + 2 * kernel_j;
+                    int input_i = i >= input_row ? i % input_row : i;
+                    int input_j = j >= input_col ? j % input_col : j;
+
                     temp += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
                     temp2 += input[input_i * input_col + input_j + 1] * kernel[kernel_i * kernel_col + kernel_j];
                     temp3 += input[(input_i + 1) * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
@@ -60,94 +44,4 @@ void singleThread(int input_row,
             output[(output_i + 1) * output_col + output_j + 1] = temp4;
         }
     }
-
-    // for (int output_i = 0; output_i < output_row; output_i += 2)
-    // {
-    //     for (int output_j = 0; output_j < output_col; output_j += 2)
-    //     {
-    //         __m128i temp1 = _mm_setzero_si128();
-    //         __m128i temp2 = _mm_setzero_si128();
-    //         __m128i temp3 = _mm_setzero_si128();
-    //         __m128i temp4 = _mm_setzero_si128();
-
-    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    //         {
-    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-
-    //                 __m128i input_values = _mm_set_epi16(input[input_i * input_col + input_j + 1],
-    //                                                      input[input_i * input_col + input_j],
-    //                                                      input[(input_i + 1) * input_col + input_j + 1],
-    //                                                      input[(input_i + 1) * input_col + input_j],
-    //                                                      0, 0, 0, 0);
-
-    //                 __m128i kernel_values = _mm_set1_epi16(kernel[kernel_i * kernel_col + kernel_j]);
-
-    //                 temp1 = _mm_add_epi32(temp1, _mm_mullo_epi16(input_values, kernel_values));
-
-    //                 // Similar calculations for temp2, temp3, and temp4
-    //             }
-    //         }
-
-    //         long long unsigned int result[4];
-    //         _mm_storeu_si128((__m128i *)result, temp1);
-    //         output[output_i * output_col + output_j] = result[0];
-    //         output[output_i * output_col + output_j + 1] = result[1];
-
-    //         // Similar store operations for temp2, temp3, and temp4
-    //     }
-    // }
-
-    // for (int output_i = 0; output_i < output_row; output_i += 2)
-    // {
-    //     for (int output_j = 0; output_j < output_col; output_j += 2)
-    //     {
-    //         for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    //         {
-    //             for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-    //                 int input_ip = (output_i + 1 + 2 * kernel_i) % input_row;
-    //                 int input_jp = (output_j + 1 + 2 * kernel_j) % input_col;
-
-    //                 output[output_i * output_col + output_j] += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 // if (output_j + 1 < output_col)
-    //                 // {
-
-    //                 output[output_i * output_col + output_j + 1] += input[input_i * input_col + input_jp] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 // }
-    //                 // if (output_i + 1 < output_row)
-    //                 // {
-
-    //                 output[(output_i + 1) * output_col + output_j] += input[(input_ip)*input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 // }
-    //                 // if (output_j + 1 < output_col && output_i + 1 < output_row)
-    //                 // {
-
-    //                 output[(output_i + 1) * output_col + output_j + 1] += input[(input_ip)*input_col + input_jp] * kernel[kernel_i * kernel_col + kernel_j];
-    //                 // }
-    //             }
-    //         }
-    //     }
-    // }
-
-    // Optimization 3
-    // for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++)
-    // {
-    //     for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++)
-    //     {
-    //         for (int output_i = 0; output_i < output_row; output_i++)
-    //         {
-    //             for (int output_j = 0; output_j < output_col; output_j++)
-    //             {
-    //                 int input_i = (output_i + 2 * kernel_i) % input_row;
-    //                 int input_j = (output_j + 2 * kernel_j) % input_col;
-    //                 output[output_i * output_col + output_j] += input[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
-    //             }
-    //         }
-    //     }
-    // }
 }
